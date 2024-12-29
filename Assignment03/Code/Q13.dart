42309208c1bf4ac7ba25148569db4a8d66cde0e9
danyalyameen <muhammaddanyalyameen@gmail.com>
2024-12-29 17:32:04 +0500

    Question 13 Solved

--- Assignment03/Code/Q13.dart
+++ Assignment03/Code/Q13.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 void main() {
-  // Print Numbers Pattern
+  // Printing Number Pattern 2
   int n = 5;
   for (var i = 1; i < n; i++) {
     for (var j = 1; j <= i; j++) {
-      stdout.write(j);
+      stdout.write(i);
     }
     print("");
   }
